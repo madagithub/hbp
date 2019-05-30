59091d1e63c528770a0f63b598f8d5c3c20065f3
Idan Kimel <idan.kimel@gmail.com>
2019-05-30 20:33:39 +0300

    Fixed log2vis running on English causing display issues by defining rtl languages

--- code/ReconstructingNeuron/Config.py
+++ code/ReconstructingNeuron/Config.py
@@ -10,10 +10,16 @@ class Config:
 		self.languageIndex = next(index for index in range(len(self.config['languages'])) if self.config['languages'][index]['prefix'] == self.languagePrefix)
 
 	def getText(self, key):
-		return pyfribidi.log2vis(self.config['texts'][self.languagePrefix][key])
+		text = self.config['texts'][self.languagePrefix][key]
+		if self.getLanguage()['rtl']:
+			text = pyfribidi.log2vis(text)
+		return text
 
 	def getTextList(self, key):
-		return [pyfribidi.log2vis(s) for s in self.config['texts'][self.languagePrefix][key].split('\n')]
+		lines = self.config['texts'][self.languagePrefix][key].split('\n')
+		if self.getLanguage()['rtl']:
+			lines = [pyfribidi.log2vis(s) for s in lines]
+		return lines
 
 	def getLanguages(self):
 		return self.config['languages']
