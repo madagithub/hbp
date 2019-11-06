28247a02e2d5955b117ee805250ea994654d01c9
Idan Kimel <idan.miller@gmail.com>
2019-11-06 18:10:19 +0200

    Fixed translation from csv script to not overwrite English

--- code/Exhibits/assets/config/update-config-translations.py
+++ code/Exhibits/assets/config/update-config-translations.py
@@ -19,7 +19,15 @@ with open('translate-pl.csv', 'r') as csvFile:
 	with open('config.json', 'r') as jsonFile:
 		data = json.load(jsonFile)
 
-	data['texts'] = texts
+	for lang in list(data['texts'].keys()):
+		if lang != 'en':
+			print('Deleting', lang)
+			del data['texts'][lang]
+
+	for lang in texts.keys():
+		if lang != 'en':
+			print('Adding', lang)
+			data['texts'][lang] = texts[lang]
 
 	with open('config-output.json', 'w') as jsonFile:
 		json.dump(data, jsonFile, ensure_ascii=False, indent='\t')
